83e4b388f23ce756eb9882a2ada5ff0d24b271ca
k-sugawa <sugawakumiko@gmail.com>
2024-07-05 00:39:30 +0900

    計測

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const file = "xlsx/sample.xlsx";
 // Function to handle file reading and displaying data
 const handleFile = async () => {
   try {
+    console.time("データ読み込み時間");
+
     const response = await fetch(file);
     const arrayBuffer = await response.arrayBuffer();
     const workbook = XLSX.read(arrayBuffer, { type: "array" });
@@ -12,6 +14,8 @@ const handleFile = async () => {
     const worksheet = workbook.Sheets[sheetName];
 
     const jsonData = XLSX.utils.sheet_to_json(worksheet);
+
+    console.timeEnd("データ読み込み時間");
     console.log(jsonData);
     return jsonData;
   } catch (error) {
@@ -21,28 +25,32 @@ const handleFile = async () => {
 };
 
 const displayData = async () => {
-    try{
-        const jsonData = await handleFile();
-        if(jsonData){
-            // Display the data in a simple HTML table
-            const table = document.createElement("table");
-            table.border = "1";
-            jsonData.forEach((row, rowIndex) => {
-              const tr = document.createElement("tr");
-              Object.values(row).forEach((cellData, cellIndex) => {
-                const td = document.createElement("td");
-                td.textContent = cellData;
-                tr.appendChild(td);
-              });
-              table.appendChild(tr);
-            });
-          
-            // Append the table to the HTML body
-            document.getElementById("excel-table").appendChild(table);
-        }
-    } catch (error) {
-        console.error("Error displaying data:", error);
+  try {
+    const jsonData = await handleFile();
+    if (jsonData) {
+      console.time("データ表示時間");
+
+      // Display the data in a simple HTML table
+      const table = document.createElement("table");
+      table.border = "1";
+      jsonData.forEach((row, rowIndex) => {
+        const tr = document.createElement("tr");
+        Object.values(row).forEach((cellData, cellIndex) => {
+          const td = document.createElement("td");
+          td.textContent = cellData;
+          tr.appendChild(td);
+        });
+        table.appendChild(tr);
+      });
+
+      // Append the table to the HTML body
+      document.getElementById("excel-table").appendChild(table);
+
+      console.timeEnd("データ表示時間");
     }
+  } catch (error) {
+    console.error("Error displaying data:", error);
+  }
 };
 
 // Call the function to display the Excel data
